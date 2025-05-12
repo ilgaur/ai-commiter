a57536abe1602b639c4d24c5d1d0d3ed63b74f72
ilgar <ilgardadashi@gmail.com>
2025-05-12 23:29:11 +0330

    chore: init hook script

---
+++ hook.py
@@ -0,0 +1,17 @@
+import sys
+
+PASS = 0
+FAIL = 1
+
+def get_diff():
+
+def analyze_diff():
+
+def main():
+    filenames = sys.argv[1:]
+
+    print(f"Files to check: {filenames}")
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
