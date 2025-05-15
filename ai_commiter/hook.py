9eb57146b4c3c62ddbf6b04d5b6264e061dac825
Ilgar Dadashi <ilgardadashi@gmail.com>
2025-05-16 00:51:07 +0330

    chore: skeleton implementation

--- ai_commiter/hook.py
+++ ai_commiter/hook.py
@@ -4,18 +4,15 @@ PASS = 0
 FAIL = 1
 
 def get_diff():
-    #IMPLEMENTATION
-    return PASS
-def create_commit_message(commit_file_path):
-    #implementation
-    return PASS
+    #returns diff data
+
+def create_commit_message(commit_file_path, diff_data):
+    #uses data from get_diff() and writes to the commit file, will return PASS or FAIL
 
 def main():
     commit_msg_file = sys.argv[1]
-
-    create_commit_message(commit_msg_file)
-
-    return PASS
+    diff_data = get_diff()
+    result = create_commit_message(commit_msg_file, diff_data)
 
 if __name__ == '__main__':
     sys.exit(main())
