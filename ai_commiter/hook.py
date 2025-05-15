2cc3b2aecc8fe40049a7b060653a473d0a30fa65
ilgar <ilgardadashi@gmail.com>
2025-05-15 23:42:25 +0330

    chore: add overall structure

---
+++ ai_commiter/hook.py
@@ -0,0 +1,21 @@
+import sys
+
+PASS = 0
+FAIL = 1
+
+def get_diff():
+    #IMPLEMENTATION
+    return PASS
+def create_commit_message(commit_file_path):
+    #implementation
+    return PASS
+
+def main():
+    commit_msg_file = sys.argv[1]
+
+    create_commit_message(commit_msg_file)
+
+    return PASS
+
+if __name__ == '__main__':
+    sys.exit(main())
