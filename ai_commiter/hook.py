9e4c3013b436e7e7153b4c4969a13e9fc6cbb2c5
Ilgar Dadashi <ilgardadashi@gmail.com>
2025-05-16 10:41:11 +0330

    fix: return the completion content for the generate commit message func and include it in the function orchestration in the main

--- ai_commiter/hook.py
+++ ai_commiter/hook.py
@@ -30,8 +30,7 @@ def generate_commit_msg(diff_data):
             }
         ]
     )
-    print(completion.choices[0].message.content)
-    pass
+    return completion.choices[0].message.content
 
 def write_commit_message(commit_file_path, commit_msg):
     #uses data from get_diff() and writes to the commit file, will return PASS or FAIL
@@ -47,7 +46,10 @@ def main():
     diff_data = get_diff()
 #   print(diff_data)
     commit_message = generate_commit_msg(diff_data)
+    if commit_message is None
+        return FAIL
     result = write_commit_message(commit_msg_file, commit_message)
+    return result
 
 if __name__ == '__main__':
     sys.exit(main())
